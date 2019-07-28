7e86e4bac813b9ca1572420234ebaf6ad61da7e4
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-07-28 17:51:53 -0500

    initial commit

---
+++ BeachBot.py
@@ -0,0 +1,102 @@
+import tweepy
+import pandas as pd
+from random import randrange
+from sodapy import Socrata
+from keys import *
+
+# authenticate to twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+# create API object
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+class MyStreamListener(tweepy.StreamListener):
+	def __init__(self, api):
+		self.api = api
+		self.user = api.me()
+
+		self.status = ''
+		self.predicted_level = 0
+		self.beach_to_parse = ''
+		self.jsonData = []
+		self.tweet = []
+
+		self.beaches = {
+			'12th': self.get_data, '12': self.get_data, '31st': self.get_data,
+			'57th':self.get_data, '57':self.get_data, 'albion':self.get_data,
+			'foster': self.get_data, 'hartigan':self.get_data, 'howard':self.get_data, 'jarvis':self.get_data,
+			'juneway':self.get_data, 'leone': self.get_data, 'margaret t burroughs': self.get_data, 'marion mahogany griffin':self.get_data,
+			'montrose': self.get_data, 'north':self.get_data, 'north avenue':self.get_data, 'oak': self.get_data,
+			'oak street': self.get_data, 'oakwood':self.get_data, 'ohio': self.get_data,
+			'osterman': self.get_data, 'rogers': self.get_data
+		}
+
+	def listen(self):
+		print(self.user.screen_name)
+		tweets_listener = MyStreamListener(api)
+		stream = tweepy.Stream(api.auth, tweets_listener)
+		stream.filter(track=["@ChicagoBeachBot"], languages=["en"])
+
+	def on_status(self, status):
+		tag_number = randrange(1, 10)   # used to prevent duplicate tweet error
+		list_of_names = list(self.beaches.keys())
+		replied = False     # breaks the loop to prevent infinite replies
+
+		self.tweet = status.text.lower().split()
+		print(self.tweet)
+
+		while not replied:
+			for word in self.tweet[1:]:  # ignores the mention
+				if word in list_of_names:
+					self.beach_to_parse = word.capitalize()
+					self.get_data()
+					if float(self.predicted_level) <= 180:
+						self.status = 'good'
+						api.update_status(
+							" {} is {}. It's predicted to be at {} today where {} is the cutoff.  #{}".
+								format(self.beach_to_parse, self.status, self.predicted_level, 235, tag_number),
+							in_reply_to_status_id=status.id, auto_populate_reply_metadata = True
+						)
+						replied = True
+					elif float(self.predicted_level) <= 220:
+						self.status = "on the edge today"
+						api.update_status(
+							" {} is {}. It's predicted to be at {} where {} is the cutoff.  #{}".
+								format(self.beach_to_parse, self.status, self.predicted_level, 235, tag_number),
+							in_reply_to_status_id=status.id, auto_populate_reply_metadata = True
+						)
+						replied = True
+					elif float(self.predicted_level) >= 235:
+						self.status = "dangerous. It should be closed."
+						api.update_status(
+							" {} is {}. It's predicted to be at {} today where {} is the cutoff.  #{}".
+								format(self.beach_to_parse, self.status, self.predicted_level, 235, tag_number),
+							in_reply_to_status_id=status.id, auto_populate_reply_metadata = True
+						)
+						replied = True
+
+	def on_error(self, status):
+		print("Error detected")
+
+	def get_data(self):
+		print('SUCCESS')
+		print('Collecting data...')
+		self.process_data(self.beach_to_parse)
+
+	def process_data(self, searchTerm):
+		client = Socrata("data.cityofchicago.org", None)
+		self.jsonData = client.get('xvsz-3xcj', limit=15, content_type='JSON')
+		print(self.jsonData, end = '\n')
+		data_frame = pd.DataFrame.from_records(self.jsonData)
+		data_frame = data_frame.set_index('beach_name') #set row labels to names
+		data_frame = data_frame[['predicted_level', 'recordid']] #gives df with names as row labels and values
+		print(data_frame)
+		selection = data_frame[data_frame.index.str.contains(searchTerm)]
+		print(selection)
+		self.predicted_level = selection['predicted_level'].iloc[0]
+		print(self.predicted_level)
+
+t = MyStreamListener(api)
+t.listen()
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Chicago-Beach-Bot
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+![Twitter URL](https://img.shields.io/twitter/url/https/ChicagoBeachBot?label=ChicagoBeachBot&style=social)
+
+This is a Twitter bot built using Tweeply for Python. This bot works on mentions to @ChicagoBeachBot using Twitter's
+streaming API. On mention, it will send a request to Chicago's SODA API (https://dev.socrata.com/foundry/data.cityofchicago.org/xvsz-3xcj)
+and parse the JSON file for the requested name of the beach. It then replies to the user with the predicted daily value of CFU.
+# Why?
+This Summer has seen an increasing amount of beach closures due to high levels of E.Coli levels measured in colony forming units/100mL. When levels exceed 235 CFU the beach is shut down. 
+The data is very easily accessible, and the City of Chicago actually provides a fairly intuitive way for people to check if a beach [is open or closed](https://www.chicagoparkdistrict.com/parks-facilities/beaches). I created this bot with the intention of making it
+easier for an average person to quickly find information on a beach using Twitter, the social platform where a bot like this actually has a use.
+# How can I make one for my city?
+If there is any similar data provided by your city through Socrata, most of the modifications should be in the `process_data` function. 
+If Socrata isn't available but there is an API, I would suggest using the requests library and working off of that with Pandas.
+The `on_status ` method is part of [Tweepy](http://docs.tweepy.org/en/latest/streaming_how_to.html), and it handles the "tweeting" functionality of the bot.
+The bot focuses on using Twitter and Tweepy's streaming api rather than fetching the data periodically. In `__init__` I initialize the beaches dictionary. This is a 
+dictionary of 'valid' beach names that is used in `on_status`. For a different city, you would just need to change the keys to the new beach names and attempt to account for 
+the short-hand way of referencing them or for the local and unofficial names. 
